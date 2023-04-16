6389f537efc685d0fb11564ec437e2c9c674da98
d2jvkpn <d2jvkpn@noreplay.noreply>
2023-04-16 14:18:51 +0800

    ...

--- internal/auth.go
+++ internal/auth.go
@@ -8,9 +8,25 @@ import (
 	"github.com/d2jvkpn/chatgpt-proxy/internal/settings"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func auth(ctx *gin.Context) {
+	ok := false
+
+	defer func() {
+		if ok {
+			return
+		}
+
+		settings.ReqLogger.Error(
+			"unauthorized access",
+			zap.String("ip", ctx.ClientIP()),
+			zap.String("method", ctx.Request.Method),
+			zap.String("url", ctx.Request.URL.String()),
+		)
+	}()
+
 	if !settings.AllowIps.Check(ctx.ClientIP()) {
 		ctx.JSON(http.StatusUnauthorized, gin.H{"code": -10, "msg": "unauthorized"})
 		ctx.Abort()
@@ -18,6 +34,7 @@ func auth(ctx *gin.Context) {
 	}
 
 	if !settings.AllowApiKeys.Enable {
+		ok = true
 		ctx.Next()
 		return
 	}
@@ -27,12 +44,15 @@ func auth(ctx *gin.Context) {
 	if auth == "" || strings.HasPrefix(auth, "Bearer ") {
 		ctx.JSON(http.StatusUnauthorized, gin.H{"code": -11, "msg": "unauthorized"})
 		ctx.Abort()
+		return
 	}
 
 	if !settings.AllowApiKeys.Check(auth[7:]) {
 		ctx.JSON(http.StatusUnauthorized, gin.H{"code": -12, "msg": "unauthorized"})
 		ctx.Abort()
+		return
 	}
 
+	ok = true
 	ctx.Next()
 }
